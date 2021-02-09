37bf921f6a8865875fd5801788c8ed6dd12910b6
Ryan <rb91827@gmail.com>
2021-02-09 12:57:05 -0600

    setup

--- Tester.java
+++ Tester.java
@@ -1,14 +1,17 @@
 
 /**
- * Write a description of class Tester here.
+ * the computer guesses what playing card the user has selected
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Ryan Brown
+ * Kolton O'Neal
+ * 2/9/21
  */
+import java.io.*;
+import java.util.*;
 public class Tester
 {
     public static void main(String args[])
     {
-        
+        Scanner kbReader = new Scanner(System.in);
     }
 }
