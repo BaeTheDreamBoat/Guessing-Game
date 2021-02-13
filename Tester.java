2fd85dda0306bbc5c32c7f426d5050c9eedb8678
Ryan <rb91827@gmail.com>
2021-02-13 08:47:33 -0600

    FInished both red and blue face branches

--- Tester.java
+++ Tester.java
@@ -28,16 +28,16 @@ public class Tester
             switch(_class) //determines if the card is a face or a number
             {
                 case 1: //face branch
-                System.out.println("Is your card a King?");
+                System.out.println("Is your card a King? (Yes/No)");
                 yesNo = kbReader.next();
                 if (yesNo.equalsIgnoreCase("yes"))
                 {
                     boolean chkKing = true; //this will help determine if card is joker
-                    System.out.println("Is the card Heats or Diamonds? (Hearts/Diamonds)");
+                    System.out.println("Is the card Hearts or Diamonds? (Hearts/Diamonds)");
                     hORd = kbReader.next();
                     if (hORd.equalsIgnoreCase("diamonds"))
                     {
-                        System.out.println("Is your card is the King of Diamonds?");
+                        System.out.println("Is your card the King of Diamonds? (Yes/No)");
                         yesNo = kbReader.next();
                         if (yesNo.equalsIgnoreCase("yes"))
                         {
@@ -51,41 +51,106 @@ public class Tester
                             System.out.print("that was not yes or no");
                     }
                     else if (hORd.equalsIgnoreCase("hearts"))
-                        System.out.print("Your card is the King of Hearts");
+                    {
+                        System.out.println("Is your the King of Hearts? (Yes/No)");
+                        yesNo = kbReader.next();
+                        if (yesNo.equalsIgnoreCase("yes"))
+                        {
+                            System.out.print("Neat");
+                            picture.loadImage("monkeyhaircut.jpeg");
+                            picture.show();
+                        }
+                        else if (yesNo.equalsIgnoreCase("no"))
+                            System.out.print("I blame the user");
+                        else
+                            System.out.print("that was not yes or no");
+                    }
                     else
                         System.out.print("That was not hearts or diamonds");
                 }
                 else if (yesNo.equalsIgnoreCase("no"))
                 {
                     boolean chkKing = false;
-                    System.out.println("Is your card a Queen?");
+                    System.out.println("Is your card a Queen? (Yes/No)");
                     yesNo = kbReader.next();
                     if (yesNo.equalsIgnoreCase("yes"))
                     {
                         boolean chkQueen = true;
-                        System.out.println("Is the card Heats or Diamonds? (Hearts/Diamonds)");
+                        System.out.println("Is the card Hearts or Diamonds? (Hearts/Diamonds)");
                         hORd = kbReader.next();
                         if (hORd.equalsIgnoreCase("diamonds"))
-                            System.out.print("Your card is the Queen of Diamonds");
+                        {
+                            System.out.println("Is your card the Queen of Diamonds? (Yes/no)");
+                            yesNo = kbReader.next();
+                            if (yesNo.equalsIgnoreCase("yes"))
+                            {
+                                System.out.print("Neat");
+                                picture.loadImage("monkeyhaircut.jpeg");
+                                picture.show();
+                            }
+                            else if (yesNo.equalsIgnoreCase("no"))
+                                System.out.print("I blame the user");
+                            else
+                                System.out.print("that was not yes or no");
+                        }
                         else if (hORd.equalsIgnoreCase("hearts"))
-                            System.out.print("Your card is the Queen of Hearts");
+                        {
+                            System.out.println("Is yor card the Queen of Hearts? (Yes/No)");
+                            yesNo = kbReader.next();
+                            if (yesNo.equalsIgnoreCase("yes"))
+                            {
+                                System.out.print("Neat");
+                                picture.loadImage("monkeyhaircut.jpeg");
+                                picture.show();
+                            }
+                            else if (yesNo.equalsIgnoreCase("no"))
+                                System.out.print("I blame the user");
+                            else
+                                System.out.print("that was not yes or no");
+                        }
                         else
                             System.out.print("That was not hearts or diamonds");
                     }
                     else if (yesNo.equalsIgnoreCase("no"))
                     {
                         boolean chkQueen = false;
-                        System.out.println("Is your card a Jack?");
+                        System.out.println("Is your card a Jack? (Yes/No)");
                         yesNo = kbReader.next();
                         if (yesNo.equalsIgnoreCase("yes"))
                         {
                             boolean chkJack = true;
-                            System.out.print("Is the card Heats or Diamonds? (Hearts/Diamonds)");
+                            System.out.println("Is the card Hearts or Diamonds? (Hearts/Diamonds)");
                             hORd = kbReader.next();
                             if (hORd.equalsIgnoreCase("diamonds"))
-                                System.out.println("Your card is the Jack of Diamonds");
+                            {
+                                System.out.println("Is your card the Jack of Diamonds? (Yes/No)");
+                                yesNo = kbReader.next();
+                                if (yesNo.equalsIgnoreCase("yes"))
+                                {
+                                    System.out.print("Neat");
+                                    picture.loadImage("monkeyhaircut.jpeg");
+                                    picture.show();
+                                }
+                                else if (yesNo.equalsIgnoreCase("no"))
+                                    System.out.print("I blame the user");
+                                else
+                                    System.out.print("that was not yes or no");
+                            }
                             else if (hORd.equalsIgnoreCase("hearts"))
-                                System.out.print("Your card is the Jack of Hearts");
+                            {
+                                System.out.println("Is your card the Jack of Hearts? (Yes/No)");
+                                yesNo = kbReader.next();
+                                if (yesNo.equalsIgnoreCase("yes"))
+                                {
+                                    System.out.print("Neat");
+                                    picture.loadImage("monkeyhaircut.jpeg");
+                                    picture.show();
+                                }
+                                else if (yesNo.equalsIgnoreCase("no"))
+                                    System.out.print("I blame the user");
+                                else
+                                    System.out.print("that was not yes or no");
+                            }
                             else
                                 System.out.print("That was not hearts or diamonds");
                         }
@@ -95,7 +160,20 @@ public class Tester
                             if (chkKing&&chkQueen&&chkJack==true)
                                 System.out.println("I dont know how you got here, as this outcome is not possible");
                             else
-                                System.out.println("Your card is the Joker");
+                            {
+                                System.out.println("Is your card the Joker? (Yes/No)");
+                                yesNo = kbReader.next();
+                                if (yesNo.equalsIgnoreCase("yes"))
+                                {
+                                    System.out.print("Neat");
+                                    picture.loadImage("monkeyhaircut.jpeg");
+                                    picture.show();
+                                }
+                                else if (yesNo.equalsIgnoreCase("no"))
+                                    System.out.print("I blame the user");
+                                else
+                                    System.out.print("that was not yes or no");
+                            }
                         }
                         else
                             System.out.print("That was not yes or no");
@@ -115,7 +193,173 @@ public class Tester
         }
         else if (color.equalsIgnoreCase("black"))
         {
+            System.out.println("Is your card a face card?\n1-yes it is a face card\n2-no it is not a face card");
+            int _class = kbReader.nextInt();
+            switch(_class) //determines if the card is a face or a number
+            {
+                case 1: //face branch
+                System.out.println("Is your card a King? (Yes/No)");
+                yesNo = kbReader.next();
+                if (yesNo.equalsIgnoreCase("yes"))
+                {
+                    boolean chkKing = true; //this will help determine if card is joker
+                    System.out.println("Is the card Spades or Clubs? (Spades/Clubs)");
+                    sORc = kbReader.next();
+                    if (sORc.equalsIgnoreCase("spades"))
+                    {
+                        System.out.println("Is your card the King of Spades? (Yes/No)");
+                        yesNo = kbReader.next();
+                        if (yesNo.equalsIgnoreCase("yes"))
+                        {
+                            System.out.print("Neat");
+                            picture.loadImage("monkeyhaircut.jpeg");
+                            picture.show();
+                        }
+                        else if (yesNo.equalsIgnoreCase("no"))
+                            System.out.print("I blame the user");
+                        else
+                            System.out.print("that was not yes or no");
+                    }
+                    else if (sORc.equalsIgnoreCase("clubs"))
+                    {
+                        System.out.println("Is your the King of Clubs? (Yes/No)");
+                        yesNo = kbReader.next();
+                        if (yesNo.equalsIgnoreCase("yes"))
+                        {
+                            System.out.print("Neat");
+                            picture.loadImage("monkeyhaircut.jpeg");
+                            picture.show();
+                        }
+                        else if (yesNo.equalsIgnoreCase("no"))
+                            System.out.print("I blame the user");
+                        else
+                            System.out.print("that was not yes or no");
+                    }
+                    else
+                        System.out.print("That was not clubs or spades");
+                }
+                else if (yesNo.equalsIgnoreCase("no"))
+                {
+                    boolean chkKing = false;
+                    System.out.println("Is your card a Queen? (Yes/No)");
+                    yesNo = kbReader.next();
+                    if (yesNo.equalsIgnoreCase("yes"))
+                    {
+                        boolean chkQueen = true;
+                        System.out.println("Is the card Clubs or Spades? (Clubs/Spades)");
+                        sORc = kbReader.next();
+                        if (sORc.equalsIgnoreCase("spades"))
+                        {
+                            System.out.println("Is your card the Queen of Spades? (Yes/no)");
+                            yesNo = kbReader.next();
+                            if (yesNo.equalsIgnoreCase("yes"))
+                            {
+                                System.out.print("Neat");
+                                picture.loadImage("monkeyhaircut.jpeg");
+                                picture.show();
+                            }
+                            else if (yesNo.equalsIgnoreCase("no"))
+                                System.out.print("I blame the user");
+                            else
+                                System.out.print("that was not yes or no");
+                        }
+                        else if (sORc.equalsIgnoreCase("clubs"))
+                        {
+                            System.out.println("Is yor card the Queen of Clubs? (Yes/No)");
+                            yesNo = kbReader.next();
+                            if (yesNo.equalsIgnoreCase("yes"))
+                            {
+                                System.out.print("Neat");
+                                picture.loadImage("monkeyhaircut.jpeg");
+                                picture.show();
+                            }
+                            else if (yesNo.equalsIgnoreCase("no"))
+                                System.out.print("I blame the user");
+                            else
+                                System.out.print("that was not yes or no");
+                        }
+                        else
+                            System.out.print("That was not hearts or spades");
+                    }
+                    else if (yesNo.equalsIgnoreCase("no"))
+                    {
+                        boolean chkQueen = false;
+                        System.out.println("Is your card a Jack? (Yes/No)");
+                        yesNo = kbReader.next();
+                        if (yesNo.equalsIgnoreCase("yes"))
+                        {
+                            boolean chkJack = true;
+                            System.out.println("Is the card Clubs or Spades? (Clubs/Spades)");
+                            sORc = kbReader.next();
+                            if (sORc.equalsIgnoreCase("spades"))
+                            {
+                                System.out.println("Is your card the Jack of Spades? (Yes/No)");
+                                yesNo = kbReader.next();
+                                if (yesNo.equalsIgnoreCase("yes"))
+                                {
+                                    System.out.print("Neat");
+                                    picture.loadImage("monkeyhaircut.jpeg");
+                                    picture.show();
+                                }
+                                else if (yesNo.equalsIgnoreCase("no"))
+                                    System.out.print("I blame the user");
+                                else
+                                    System.out.print("that was not yes or no");
+                            }
+                            else if (sORc.equalsIgnoreCase("clubs"))
+                            {
+                                System.out.println("Is your card the Jack of Clubs? (Yes/No)");
+                                yesNo = kbReader.next();
+                                if (yesNo.equalsIgnoreCase("yes"))
+                                {
+                                    System.out.print("Neat");
+                                    picture.loadImage("monkeyhaircut.jpeg");
+                                    picture.show();
+                                }
+                                else if (yesNo.equalsIgnoreCase("no"))
+                                    System.out.print("I blame the user");
+                                else
+                                    System.out.print("that was not yes or no");
+                            }
+                            else
+                                System.out.print("That was not clubs or spades");
+                        }
+                        else if (yesNo.equalsIgnoreCase("no"))
+                        {
+                            boolean chkJack = false;
+                            if (chkKing&&chkQueen&&chkJack==true)
+                                System.out.println("I dont know how you got here, as this outcome is not possible");
+                            else
+                            {
+                                System.out.println("Is your card the Joker? (Yes/No)");
+                                yesNo = kbReader.next();
+                                if (yesNo.equalsIgnoreCase("yes"))
+                                {
+                                    System.out.print("Neat");
+                                    picture.loadImage("monkeyhaircut.jpeg");
+                                    picture.show();
+                                }
+                                else if (yesNo.equalsIgnoreCase("no"))
+                                    System.out.print("I blame the user");
+                                else
+                                    System.out.print("that was not yes or no");
+                            }
+                        }
+                        else
+                            System.out.print("That was not yes or no");
+                    }
+                    else
+                        System.out.print("That was not yes or no");
+                }
+                else
+                    System.out.print("That was not yes or no");
+                break;
+                case 2: //number branch
 
+                break;
+                default:
+                System.out.println("That was not the numbers 1 or 2");
+            }
         }   
     }
 }
